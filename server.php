eb1712d24842fe44936869dfff3d046f59925ea6
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-01-27 18:43:33 +0100

    Fix server

--- server.php
+++ server.php
@@ -1,11 +1,9 @@
-<?php 
+<?php
 $getJson = file_get_contents(__DIR__ . '/db/dischi.json');
-$array = json_decode($getJson,true);
+$array = json_decode($getJson, true);
 
-// if ($array != null) {
-//  }
-//  var_dump($array);
-header('Content-Type: application/json');
-echo json_encode($array);
+if ($array != null) {
+    header('Content-Type: application/json');
 
-?>
+    echo json_encode($array);
+}
